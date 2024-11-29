207a1a0f3760bec9f97975ea82548fb0a792b9f7
tiagoolindo2 <lu.nabuco.sil@gmail.com>
2024-11-29 15:25:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Kurapika',
+    'Quem e o Kurapika?',
+    'Kurapika é o último membro restante do Clã Kurta (クルタ族, Kuruta-zoku ) que deseja se tornar um Hunter para vingar seu clã e recuperar seus olhos brilhantes escarlates que foram arrancados de seus cadáveres por um bando de ladrões conhecido como Phantom Troupe.'
+)
+
+criaCartao(
+    'Luffy',
+    'Quem e Monkey D Luffy?',
+    'Ele é apresentado como um jovem cujo corpo ganha as propriedades de borracha após ter comido a Gomu-Gomu no Mi, uma das várias frutas amaldiçoadas conhecidas como Akuma no Mi ou Frutas do Diabo. '
+)
+
+criaCartao(
+    'Naruto',
+    'quem e Naruto?',
+    'Naruto Uzumaki, um jovem órfão habitante da Aldeia da Folha que sonha em se tornar o quinto Hokage, o maior guerreiro e governante da vila.uma menina inteligente e gosta de ouvir piseiro e gosta de girasol e ela que um cuelho'
+)
+
+criaCartao(
+    'Goku',
+    'quem e Goku?',
+    'cujo nome de nascimento é Kakarotto, é o protagonista da franquia Dragon Ball, criada por Akira Toriyama. Sua primeira aparição ocorreu no primeiro capítulo do mangá Dragon Ball, intitulado Bulma e Son Goku, publicado na revista Weekly Shōnen Jump em 3 de dezembro de 1984.'
+)
